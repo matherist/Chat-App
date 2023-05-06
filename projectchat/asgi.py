33b3de55fb29d24e388a93822fd0b101a03e54c3
Dinaiym <dinaiym.dubanaeva@alatoo.edu.kg>
2023-05-06 12:15:01 +0600

    Sign Up added

---
+++ projectchat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for projectchat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectchat.settings')
+
+application = get_asgi_application()
