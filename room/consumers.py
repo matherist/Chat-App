e66c4cc5f77a9f62ac033a66040f44e7c0c777fd
matherist <128795754+matherist@users.noreply.github.com>
2023-05-10 11:42:43 +0600

    Update consumers.py

--- room/consumers.py
+++ room/consumers.py
@@ -3,9 +3,14 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from .models import Message, Room
 from django.contrib.auth.models import User
-from textblob import TextBlob  # Импортируйте TextBlob
+from textblob import TextBlob
 import re
-# Code
+from better_profanity import profanity
+from .bad_words import bad_words
+
+# Load the bad words into the profanity filter
+profanity.load_censor_words(bad_words)
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -17,10 +22,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
         print(f"WebSocket connected for room {self.room_name}")
-        # ... остальной код
 
-    async def disconnect(self):
-        # ... остальной код
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -32,7 +35,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
-        # Замените плохие слова звездочками
         filtered_message = await self.filter_bad_words(message)
 
         await self.save_message(username, room, filtered_message)
@@ -46,10 +48,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def chat_message(self, event):
-        # ... остальной код
-        message = event['message'],
-        username = event['username'],
-        room = event['room'],
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
         await self.send(text_data=json.dumps({
             'message': message,
             'username': username,
@@ -58,21 +60,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def save_message(self, username, room, message):
-        # ... остальной код
-        user = User.objects.get(username=username)  # Add the keyword argument here
+        user = User.objects.get(username=username)
         room = Room.objects.get(slug=room)
-
         Message.objects.create(user=user, room=room, content=message)
 
-    # Функция для обработки и замены плохих слов с использованием NLP
     async def filter_bad_words(self, message):
-        # Определите тональность сообщения с помощью TextBlob
         sentiment = TextBlob(message).sentiment.polarity
+        contains_profanity = profanity.contains_profanity(message)
 
-        # Замените плохие слова звездочками, если тональность сообщения негативна
-        if sentiment < 0:
-            # Можете использовать любой алгоритм замены плохих слов звездочками
-            # Здесь пример простого регулярного выражения для замены слов
-            message = re.sub(r'\b\w+\b', '*****', message)
-
+        if sentiment < 0 or contains_profanity:
+            message = '*' * len(message)
         return message
